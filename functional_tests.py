41a3d0b62a1b2e7a055b736dd4dcee42f220919e
achang0121 <13901406174@163.com>
2020-08-20 12:49:28 +0800

    refactor 2

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+    
     def test_can_start_a_list_and_retrieve_it_later(self):
         
         # 伊利斯听说有一个很cool的在线待办事项应用
@@ -37,24 +42,25 @@ class NewVisitorTest(unittest.TestCase):
         # 待办事项表格中显示了“1: Buy peacock feathers”
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了“Use peacock feathers to make a fly”
         # 伊利斯做事很有条理
-        self.fail('Finish the test~')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
         
         # 页面再次更新，她的清单中显示了两个待办事项
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # 伊利斯想知道这个网站是否会记住她的清单
         # 她看到网站为她生成了一个唯一的URL
         # 而且页面中有一些文字解说这个功能
 
+        self.fail('Finish the test~')
         # 她访问那个URL，发现她的待办事项列表还在
 
         # 她很满意，去睡觉了
